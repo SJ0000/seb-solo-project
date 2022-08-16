ee42c7cc9bdf573d002e717168683b2fe926b298
sj <slaayer60@gmail.com>
2022-08-16 10:41:22 +0900

    init

---
+++ src/main/java/me/sj/sebsoloproject/SebSoloProjectApplication.java
@@ -0,0 +1,13 @@
+package me.sj.sebsoloproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SebSoloProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SebSoloProjectApplication.class, args);
+	}
+
+}
